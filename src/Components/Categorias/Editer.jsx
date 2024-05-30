6bcc7fefa55e08fb5d46083c79b5bd6f28d93ec1
Fras28 <fselvarolo28@gmail.com>
2024-05-30 12:26:43 -0300

    actualizacion

--- src/Components/Categorias/Editer.jsx
+++ src/Components/Categorias/Editer.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CardsEdite } from "../Cards/CardsEdit.jsx";
 import { asyncAllProducts } from "../redux/slice.jsx";
-import AddProduct from "../Comander/formVenta/formAddProd.jsx";
 import ModalGen from "../Modal/ModalConfirmacion/Modal.jsx";
-
+import AddProduct from "../Comander/formVenta/formAddProd.jsx";
 
 export const Editer = () => {
   const dispatch = useDispatch();
@@ -14,25 +13,21 @@ export const Editer = () => {
     dispatch(asyncAllProducts());
   }, []);
 
-  // Estado para el valor de búsqueda
   const [searchValue, setSearchValue] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  // Efecto secundario para actualizar filteredProducts cuando allProduct cambie
   useEffect(() => {
     setFilteredProducts(allProduct);
-  }, []);
+  }, [allProduct]);
 
   const handleSearchChange = (e) => {
     const value = e?.target?.value;
     setSearchValue(value);
 
-    // Si el valor de búsqueda está vacío, enviar directamente allProduct
     if (value === "") {
       setFilteredProducts(allProduct);
     } else {
-      // Filtrar los productos basados en el valor de búsqueda
-      const filtered = allProduct?.filter(product =>
+      const filtered = allProduct.filter(product =>
         product?.attributes?.name?.toLowerCase().includes(value.toLowerCase())
       );
       setFilteredProducts(filtered);
@@ -41,7 +36,7 @@ export const Editer = () => {
 
   return (
     <div className="containerEdit">
-        <div className="searchBar">
+      <div className="searchBar">
         <label htmlFor="">Buscar</label>
         <input
           type="search"
@@ -51,7 +46,7 @@ export const Editer = () => {
           onChange={handleSearchChange}
           placeholder="Buscar producto..."
         />
-        </div>
+      </div>
       <div>
         <h2 style={{paddingTop:"2.5rem"}}>Edicion de productos</h2>
         <ModalGen Child={<AddProduct />} txtBtn="+ Producto" />
