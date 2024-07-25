41a032c4e4669c0a4777f4f4f759f192058c0439
Fras28 <fselvarolo28@gmail.com>
2024-07-25 10:35:23 -0300

    funcionalidad botones secctions

--- src/Components/Categorias/CompSubCat.jsx
+++ src/Components/Categorias/CompSubCat.jsx
@@ -11,16 +11,15 @@ import { asyncSubCategoria } from "../redux/slice.jsx";
 const API = process.env.REACT_APP_API_STRAPI;
 
 export const CompSubCat = ({ idCat }) => {
-  const { id } = useParams(); // Usa el hook useParams para obtener el parámetro de la URL
+  const { id } = useParams();
   const dispatch = useDispatch();
   const { allProduct, subCategorias, categorias } = useSelector(
     (state) => state.alldata
   );
 
   useEffect(() => {
-    // Función que se ejecutará cuando el componente se monte o cuando id cambie
     dispatch(asyncSubCategoria(idCat));
-  }, [dispatch, id]); // Dependencias: dispatch y id
+  }, [dispatch, id]);
 
   const articulosParaFiltrar = subCategorias.filter((e) => e.id === idCat);
   const articulos =
@@ -45,18 +44,17 @@ export const CompSubCat = ({ idCat }) => {
     return acc;
   }, []);
 
-  // Puedes acceder a cada Xn dinámicamente
   const dynamicVariables = Object.keys(subCategoriaFilters).map((key) => {
     return subCategoriaFilters[key];
   });
   const processedNames = articulos
-    .filter((product) => product.attributes.articulos.data.length !== 0) // Filtrar productos con datos de artículos
+    .filter((product) => product.attributes.articulos.data.length !== 0)
     .map((product) => {
       const productName = product.attributes.name;
       return productName
-        .split(/\[.*?\]|\(.*?\)/) // Dividir la cadena usando corchetes [] y paréntesis ()
+        .split(/\[.*?\]|\(.*?\)/)
         .map((part) => part.trim())
-        .join(""); // Unir las partes filtradas en una sola cadena
+        .join("");
     });
   const img1 = categorias.filter((e) => e.id === idCat);
   const urlImg = img1[0]?.attributes?.picture?.data?.attributes?.url;
@@ -65,25 +63,26 @@ export const CompSubCat = ({ idCat }) => {
     <div className="containerL">
       <Nav id={id} />
 
-        {articulos?.length > 0 ? (
-          <div className="sectioner">
-            <p> Secciones : </p>
-            {processedNames.length > 0 &&
-              processedNames.map((name, index) => (
-                <a key={index} href={`#${articulos[index].id}`}>
-                  {name}
-                </a>
-              ))}
-          </div>
-        ) : null}
+      {articulos?.length > 0 ? (
+        <div className="sectioner">
+          <p> Secciones : </p>
+          {processedNames.length > 0 &&
+            processedNames.map((name, index) => (
+              <a key={index} href={`#section-${articulos[index].id}`}>
+                {name}
+              </a>
+            ))}
+        </div>
+      ) : null}
  
       <div className="conteinerLC ">
         {articulos?.length > 0 ? (
           <div className="conteinerLB2 animate__animated  animate__zoomIn animate__faster">
             <div className="conteinerLB2 animate__animated animate__zoomIn animate__faster">
               {articulos?.map((prod, index) => (
-                <div>
-                  {index === 0 ? (
+                <div key={prod.id}>
+                  <div id={`section-${prod.id}`} style={{ height: index === 0 ? "0" : "110px" }}></div>
+                  {index === 0 && (
                     <div
                       style={{
                         display: "flex",
@@ -93,8 +92,7 @@ export const CompSubCat = ({ idCat }) => {
                         overflow: "hidden",
                         position: "relative",
                         backgroundColor: "white",
-                        outline:"solid orange 2px",
-  
+                        outline: "solid orange 2px",
                       }}
                     >
                       <img
@@ -102,18 +100,14 @@ export const CompSubCat = ({ idCat }) => {
                         alt=""
                         style={{
                           position: "absolute",
-                          top: "40%", // Ajusta este valor para mover la imagen verticalmente
-                          left: "50%", // Ajusta este valor para mover la imagen horizontalmente
-                          transform: "translate(-50%, -50%)", // Para centrar la imagen
+                          top: "40%",
+                          left: "50%",
+                          transform: "translate(-50%, -50%)",
                           width: "100%",
-                    
                         }}
                       />
                     </div>
-                  ) : (
-                    <div id={prod.id} style={{ height: "110px" }}></div>
                   )}
-
                   <Cards products={prod} />
                 </div>
               ))}
